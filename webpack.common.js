0f6b783fe681c366dd0a504df8670a829a403ca8
Wang Xiaoqiang <xiaoqiang.wang@storehub.com>
2019-01-28 10:45:50 +0800

    remove babels, add axios

--- webpack.common.js
+++ webpack.common.js
@@ -17,9 +17,9 @@ module.exports = {
     module: {
         rules: [
             {
-                // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+                // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
                 test: /\.tsx?/,
-                loader: 'awesome-typescript-loader',
+                loader: 'ts-loader',
             },
             {
                 // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
